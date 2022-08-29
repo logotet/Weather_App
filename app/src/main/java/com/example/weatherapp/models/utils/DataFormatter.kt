d828c9648566fed8dc272fabc2dd5b509e3a67bc
v.vasilev <v.vasilev@up2technology.com>
2022-08-29 16:01:53 +0300

    Add weather data from viewModel to Screen

--- app/src/main/java/com/example/weatherapp/models/utils/DataFormatter.kt
+++ app/src/main/java/com/example/weatherapp/models/utils/DataFormatter.kt
@@ -1,5 +1,7 @@
 package com.example.weatherapp.models.utils
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.example.weatherapp.R
 import com.example.weatherapp.models.measure.UnitSystem
 import com.example.weatherapp.utils.ResourceProvider
@@ -23,8 +25,28 @@ fun Double?.formatTemperature(
     )
 }
 
+@Composable
+fun Double?.formatTemperatureComposable(
+    unitSystem: UnitSystem,
+): String {
+    val tempFormat = when (unitSystem) {
+        UnitSystem.METRIC -> stringResource(R.string.celsius_temperature_format)
+        UnitSystem.STANDARD -> stringResource(R.string.standard_temperature_format)
+        UnitSystem.IMPERIAL -> stringResource(R.string.imperial_temperature_format)
+    }
+    return stringResource(
+        R.string.temperature_format,
+        this ?: 0.0,
+        tempFormat
+    )
+}
+
 fun Int?.formatHumidity(resourceProvider: ResourceProvider): String =
     resourceProvider.getString(R.string.humidity_format, this ?: 0)
+
+@Composable
+fun Int?.formatHumidityComposable(): String =
+    stringResource(R.string.humidity_format, this ?: 0)
 
 fun Double?.formatSpeed(resourceProvider: ResourceProvider, unitSystem: UnitSystem): String {
     val speedFormat = when (unitSystem) {
@@ -33,6 +55,16 @@ fun Double?.formatSpeed(resourceProvider: ResourceProvider, unitSystem: UnitSyst
         UnitSystem.IMPERIAL -> resourceProvider.getString(R.string.imperial_speed_format)
     }
     return resourceProvider.getString(R.string.speed_format, this ?: 0.0, speedFormat)
+}
+
+@Composable
+fun Double?.formatSpeedComposable(unitSystem: UnitSystem): String {
+    val speedFormat = when (unitSystem) {
+        UnitSystem.METRIC -> stringResource(id = R.string.metric_standard_speed_format)
+        UnitSystem.STANDARD -> stringResource(id = R.string.standard_temperature_format)
+        UnitSystem.IMPERIAL -> stringResource(id = R.string.imperial_speed_format)
+    }
+    return stringResource(R.string.speed_format, this ?: 0.0, speedFormat)
 }
 
 fun Long.formatHour(timeOffset: Int): String {
