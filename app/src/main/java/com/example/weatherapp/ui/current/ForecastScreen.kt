46385e4925ad460c28142de1d41518ca274c3553
v.vasilev <v.vasilev@up2technology.com>
2022-10-12 11:18:26 +0300

    Add preview screens

--- app/src/main/java/com/example/weatherapp/ui/current/ForecastScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/current/ForecastScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,7 @@ import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.weatherapp.R
 import com.example.weatherapp.models.measure.UnitSystem
+import com.example.weatherapp.models.ui.CurrentWeatherModel
 import com.example.weatherapp.models.utils.*
 import com.example.weatherapp.ui.Appbar
 import com.google.android.gms.maps.model.CameraPosition
@@ -352,3 +354,160 @@ fun Hour(
         }
     }
 }
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ForecastScreenPreview() {
+    Surface(
+        modifier = Modifier,
+//            .padding(it)
+        color = colorResource(id = R.color.jordi_blue)
+    ) {
+        Column {
+            CurrentWeatherDataPreview()
+            HourListPreview()
+            MapPreview()
+        }
+    }
+}
+
+@Composable
+fun CurrentWeatherDataPreview() {
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(8.dp)
+    ) {
+        val model = CurrentWeatherModel(
+            name = "Sofia",
+            description = "broken clouds",
+            temperature = 300.0,
+            windSpeed = 5.1,
+            humidity = 25,
+            icon = "2n",
+            lat = 23.21,
+            lon = 43.46,
+            windDirection = 280,
+        )
+
+        val unitSystem = UnitSystem.METRIC
+
+        TextAndIconForecast(
+            painter = painterResource(id = R.drawable.ic_location_name),
+            text = model.name,
+            iconSize = 50.dp,
+        )
+
+        val formattedTemperature = unitSystem.mapTemperature(model.temperature)
+            .formatTemperatureComposable(unitSystem = unitSystem)
+        TextAndIconForecast(
+            painter = rememberAsyncImagePainter(model.icon.getIconUrl()),
+            text = formattedTemperature,
+            tint = Color.Unspecified,
+            rowHeight = 80.dp,
+            iconSize = 100.dp,
+            fontSize = 44.sp
+        )
+
+        Text(
+            text = model.description.capitalizeFirstChar(),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            fontSize = 28.sp,
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextAndIconForecast(
+                modifier = Modifier.fillMaxWidth(0.3f),
+                painter = painterResource(id = R.drawable.ic_raindrop),
+                text = model.humidity.formatHumidityComposable()
+            )
+
+            TextAndIconForecast(
+                modifier = Modifier.fillMaxWidth(0.6f),
+                iconModifier = Modifier
+                    .padding(end = 6.dp)
+                    .size(32.dp)
+                    .fillMaxSize()
+                    .align(Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_arrow_direction),
+                text = model.windSpeed.formatSpeedComposable(unitSystem),
+                rotation = model.windDirection
+            )
+        }
+
+        Text(
+            text = stringResource(id = R.string.hourly),
+            fontSize = 16.sp,
+            color = Color.White,
+            modifier = Modifier.padding(
+                top = 20.dp,
+                start = 4.dp,
+                bottom = 4.dp
+            )
+        )
+    }
+}
+
+@Composable
+fun HourListPreview() {
+    Surface(
+        shape = RoundedCornerShape(
+            topStart = 10.dp,
+            topEnd = 10.dp,
+            bottomEnd = 10.dp,
+            bottomStart = 10.dp
+        )
+    ) {
+        LazyRow {
+            items(7) {
+                Hour(
+                    hour = 10,
+                    timeOffset = 3,
+                    iconCode = "2n",
+                    temperature = 300.0,
+                    humidity = 20,
+                    windSpeed = 20.0,
+                    rotation = 20,
+                    unitSystem = UnitSystem.METRIC
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MapPreview() {
+    val location = LatLng(
+        24.21,
+        23.45
+    )
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(
+            location,
+            10f
+        )
+    }
+
+    GoogleMap(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+            .padding(top = 10.dp),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            position = location,
+        )
+    }
+}
+
